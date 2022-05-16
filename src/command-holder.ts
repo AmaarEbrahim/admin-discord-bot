9c8d5624df67522dee37fd44acc13385eb219be2
Josh <joshconklin926@gmail.com>
2022-05-16 18:25:36 -0400

    check command-holder

--- src/command-holder.ts
+++ src/command-holder.ts
@@ -4,6 +4,9 @@ import { temp_mute } from "./commands/mute"
 import { PingCommand } from "./commands/ping"
 import { SetupCommand } from "./commands/setup"
 
+
+// wackey do you see this
+
 export type TBotCommand = {
     data: SlashCommandBuilder
     run: (interaction: CommandInteraction<CacheType>) => Promise<unknown>
